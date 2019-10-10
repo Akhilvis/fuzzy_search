33cbc1a92672186d83d6517d7dbf0d779d2c9ccf
akhilvis <akhilviswam000@gmail.com>
2019-10-10 23:17:56 +0530

    jqury ui added

--- fuzzy_search/fuzzy_search/urls.py
+++ fuzzy_search/fuzzy_search/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
+from fuzzyapp import  urls as app_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('autocomplete/', include(app_urls)),
+
 ]
