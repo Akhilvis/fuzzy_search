9ee9b2ad3998767985c72e0893de8899b8c5a320
akhilvis <akhilviswam000@gmail.com>
2019-10-10 23:52:48 +0530

    basic flow setup

--- fuzzy_search/fuzzyapp/views.py
+++ fuzzy_search/fuzzyapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
 
 # Create your views here.
 
@@ -8,3 +9,30 @@ def home(request):
 def fuzzy_search(request, *args, **kwargs):
     print ('fuzzy_search......................',request.GET.get('word', -1))
     word = request.GET.get('word', '')
+
+    autocomplete = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ]
+
+    return JsonResponse({'success': True, 'autocomplete': autocomplete})
