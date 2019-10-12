008a53e18c3542975fdb434f1abafc29c4870698
akhilvis <akhilviswam000@gmail.com>
2019-10-12 09:52:40 +0530

    basic fuzzy logic added

--- fuzzy_search/fuzzyapp/views.py
+++ fuzzy_search/fuzzyapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
 import re     # regex module from standard library.
-
+import csv
 
 # Create your views here.
 
@@ -38,7 +38,12 @@ def fuzzy_search(request, *args, **kwargs):
         "Scheme"
     ]
 
-    return JsonResponse({'success': True, 'autocomplete': autocomplete})
+    dict_list = tsv_file_reader()
+    auto_list = fuzzyfinder(word, dict_list)[:10]
+    print (auto_list)
+
+
+    return JsonResponse({'success': True, 'autocomplete': auto_list})
 
 
 def fuzzyfinder(user_input, collection):
@@ -50,3 +55,12 @@ def fuzzyfinder(user_input, collection):
         if match:
             suggestions.append((len(match.group()), match.start(), item))
     return [x for _, _, x in sorted(suggestions)]
+
+def tsv_file_reader():
+    dict_list = []
+    with open("fuzzyapp/word_search.tsv") as tsvfile:
+        tsvreader = csv.reader(tsvfile, delimiter="\t")
+        for line in tsvreader:
+            dict_list.append(line[0])
+
+    return dict_list
